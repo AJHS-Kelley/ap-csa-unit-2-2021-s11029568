5de68d8719b0eb6d4c7482ec275e62f7a2e27c4f
Venti <70521040+s11029568@users.noreply.github.com>
2021-11-03 09:09:23 -0400

    1.0

---
+++ Student.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+// Unit 2, Topic 2.1, Classes and Objects, 11/03/21
+
+
+public class Student {
+    // these are INSTANCE VARIABLES.
+    private int studentID;
+    private int gradeLevel;
+    private double gradePointAvg;
+    private String studentName;
+
+
+    // Creating a Constructor
+    public Student() {}
