eab5cd966e66cd780b812f3d4aad2bce6b8c2450
Venti <70521040+s11029568@users.noreply.github.com>
2021-11-15 09:00:33 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -95,6 +95,31 @@ public class Student {
             System.out.println(lunchPeriod);
         }
     }
+    public static void assignlunchmultiple(int gradeLevel, double gradePointAvg) {
+        String lunchPeriod; 
+        // AND, OR, NOT -- Boolean Operators
+        // && = AND
+        // || = OR
+        // !  = NOT
+
+        if (gradeLevel == 9 || gradePointAvg <= 1.5) {
+            lunchPeriod = "First Lunch";
+            System.out.print("This student has");
+            System.out.println(lunchPeriod);
+        } else if (gradeLevel == 10 && gradePointAvg > 2.0) {
+            lunchPeriod = "Second Lunch";
+            System.out.print("This student has");
+            System.out.println(lunchPeriod);
+        } else if (gradeLevel == 11 && (gradePointAvg != 0.0)) {
+            lunchPeriod = "Second Lunch";
+            System.out.print("This student has");
+            System.out.println(lunchPeriod);
+        } else {
+            lunchPeriod = "Third Lunch";
+            System.out.print("This student has");
+            System.out.println(lunchPeriod);
+        }
+    }
 
     public static void main(String[] args) {
         Student student0 = new Student();
